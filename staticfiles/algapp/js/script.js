88aa6c6a3958dd1be384289e7ae9aa1aef7f9a4c
Sergeisbq <ss_b@icloud.com>
2023-07-03 23:23:54 +0300

    1

--- staticfiles/algapp/js/script.js
+++
@@ -1,8 +0,0 @@
-let h2 = document.getElementsByTagName('h2');
-h2[0].style.textAlign = 'center';
-let div = document.getElementById('container_re');
-div.style.display = 'flex';
-div.style.flexDirection = 'row';
-div.style.justifyContent = 'space-around';
-div.style.margin = '0 auto';
-
